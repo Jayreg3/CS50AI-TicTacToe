7a079a2933fe9878cc2f20cce0defbd4772fe140
Jayreg3 <jeregistre@gmail.com>
2020-12-27 22:29:57 -0500

    Made optimizations, but solution still does not work. To be continued later...

--- tictactoe copia.py
+++ tictactoe copia.py
@@ -23,44 +23,54 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    is_x_turn = 0
+
+    # Subtracts the number of Os from the number of Xs (X always starts first)
+    x_minus_o = 0
     for row in board:
         for column in row:
             if column == 'X':
-                is_x_turn += 1
+                x_minus_o += 1
             elif column == 'O':
-                is_x_turn -= 1
+                x_minus_o -= 1
             else:
                 continue
-    return 'X' if is_x_turn == 0 else 'O'
+    # Same number of X and O means X's turn
+    return X if x_minus_o == 0 else O
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    actions = set()
+    possible_actions = set()
     for i, row in enumerate(board):
         for j, column in enumerate(row):
             if column == None:
-                actions.add((i, j))
-    return actions
+                possible_actions.add((i, j))
+    return possible_actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    current_player = player(board)
+    # Deep copy the current board to not manipulate the input board
     resulting_board = copy.deepcopy(board)
-    if resulting_board[action[0]][action[1]] is not None:
-        try:
-            raise Exception
-        except Exception as err:
-            if not err.args:
-                err.args=('',)
-            err.args += ("Action not valid. The spot already has a mark. Try another action",)
-            raise
+    # Raise an exception if the intended spot is already taken or out of the board
+    if resulting_board[action[0]][action[1]] is not None or action[0] not in range(0,3) or action[1] not in range(0,3):
+        # More complex way to raise exception
+        # try:
+        #     raise Exception
+        # except Exception as err:
+        #     if not err.args:
+        #         err.args=('',)
+        #     err.args += ("Action not valid. The spot already has a mark or is out of the board. Try another action",)
+        #     raise
+
+        # Simpler way to raise exception
+        raise Exception("Action not valid. The spot already has a mark or is out of the board. Try another action")
+    # If the move is legal, sets the current player's mark at the corresponding cell
+    current_player = player(board)
     resulting_board[action[0]][action[1]] = current_player
     return resulting_board
 
@@ -69,10 +79,13 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
+    # Creates a dict to tally each player's number in a row by row, column, or diagonal
     tally = {'Xtop_left_diagonal':0, 'Xtop_right_diagonal':0, 'Otop_left_diagonal':0, 'Otop_right_diagonal':0}
+    # Define what is considered a diagonal
     top_left_diagonal = ((0,0), (1,1), (2,2))
     top_right_diagonal = ((2,0), (1,1), (0,2))
 
+    # Loops through board, adding to the player's tally for each corresponding row, column, and diagonal
     for i,row in enumerate(board):
         for j,column in enumerate(row):
             if column == None:
@@ -89,8 +102,10 @@ def winner(board):
                     tally[top_left_key] += 1
                 if (i,j) in top_right_diagonal:
                     tally[top_right_key] += 1
+                # Report a winner as soon as any tally reaches 3
                 if 3 in [tally[col_key], tally[row_key], tally[top_left_key], tally[top_right_key]]:
                     return player
+    # Return None if no winner is found; game is in progress or tied
     return None
 
 
@@ -98,10 +113,18 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    if winner(board) is None and len(actions(board)) > 0:
-        return False
-    else:
+    # Less optimal solution that calls the actions function which loops through the board twice
+    # if winner(board) is None and len(actions(board)) > 0:
+    #     return False
+    # else:
+    #     return True
+
+    # More optimal solution that loops through the board once
+    # Returns true (game over) if there is a winner or there are no empty spots
+    if winner(board) is not None or not any(spot == EMPTY for spot in [col for row in board for col in row]):
         return True
+    else:
+        return False
 
 
 def utility(board):
@@ -122,42 +145,46 @@ def minimax(board):
     """
     if terminal(board) is True:
         return None
-    best_action = {'action': False, 'value': False, 'num_of_turns': float('inf')}
+
+    # Small optimization: if no move has been made, start in the middle
+    if board == initial_state():
+        return (1,1)
+
+    best_action = {'action': False, 'value': False}
 
     if player(board) == 'X':
         for action in actions(board):
             action_value = max_value(result(board, action), 1)
-            if (action_value['value'] >= best_action['value'] and action_value['num_of_turns'] <= best_action['num_of_turns']) or best_action['value'] is None:
+            if action_value > best_action['value'] or best_action['value'] is None:
                 best_action['action'] = action
-                best_action['value'] = action_value['value']
-                best_action['num_of_turns'] = action_value['num_of_turns']
+                best_action['value'] = action_value
             print(best_action)
     else:
         for action in actions(board):
             action_value = min_value(result(board, action), 1)
-            if (action_value['value'] <= best_action['value'] and action_value['num_of_turns'] <= best_action['num_of_turns']) or best_action['value'] is None:
+            if action_value < best_action['value'] or best_action['value'] is None:
                 best_action['action'] = action
-                best_action['value'] = action_value['value']
-                best_action['num_of_turns'] = action_value['num_of_turns']
+                best_action['value'] = action_value
             print(best_action)
     return best_action['action']
 
 
 def max_value(board, num_of_turns):
-    print('running max_value with board = ', board, ' and num_of_turns = ', num_of_turns)
     if terminal(board):
-        return {'value': utility(board), 'num_of_turns': num_of_turns}
+        return utility(board) / num_of_turns
     value = float('-inf')
     for action in actions(board):
-        value = max(value, min_value(result(board, action), num_of_turns + 1)['value'])
-    return {'value': value, 'num_of_turns': num_of_turns}
+        value = max(value, min_value(result(board, action), num_of_turns + 1))
+        print('running max_value with board = ', board, ' value is ', value)
+    return value
 
 
 def min_value(board, num_of_turns):
-    print('running min_value with board = ', board, ' and num_of_turns = ', num_of_turns)
     if terminal(board):
-        return {'value': utility(board), 'num_of_turns': num_of_turns}
+        return utility(board) / num_of_turns
     value = float('inf')
     for action in actions(board):
-        value = min(value, max_value(result(board, action), num_of_turns + 1)['value'])
-    return {'value': value, 'num_of_turns': num_of_turns}
+        value = min(value, max_value(result(board, action), num_of_turns + 1))
+        print('running max_value with board = ', board, ' value is ', value)
+    return value
+
